b6c163e9addc7ac8794307b2f684e1f208a05058
gabrielgelen-cps <141190081+gabrielgelen-cps@users.noreply.github.com>
2024-06-24 20:03:30 -0300

    Conferencia 0624

--- ideiaapp/routes/authRoutes.js
+++ ideiaapp/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const AuthController = require('../controllers/AuthController')
 
 router.get('/login', AuthController.login)
-router.post('loginPost', AuthController.loginPost)
+router.post('/loginPost', AuthController.loginPost)
 router.get('/register', AuthController.register)
 router.post('/register', AuthController.registerPost)
 router.get('/logout', AuthController.logout)
